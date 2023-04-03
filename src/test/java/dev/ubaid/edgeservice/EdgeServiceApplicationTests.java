c6dfc3fe29a5b1cc50ce9a84a7aa3ef8312c13b8
ubaid4j <ubaidshan007@gmail.com>
2023-04-03 12:55:38 +0500

    add context verification test

--- src/test/java/dev/ubaid/edgeservice/EdgeServiceApplicationTests.java
+++ src/test/java/dev/ubaid/edgeservice/EdgeServiceApplicationTests.java
@@ -2,10 +2,33 @@ package dev.ubaid.edgeservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
-@SpringBootTest
+@SpringBootTest(
+	webEnvironment = WebEnvironment.RANDOM_PORT
+)
+@Testcontainers
 class EdgeServiceApplicationTests {
 
+	private static final int REDIS_PORT = 6379;
+
+	@Container
+	private static GenericContainer<?> redis =
+		new GenericContainer<>(DockerImageName.parse("redis:latest"))
+			.withExposedPorts(REDIS_PORT);
+	
+	@DynamicPropertySource
+	private static void redisProps(DynamicPropertyRegistry registry) {
+		registry.add("spring.data.redis.host", () -> redis.getHost());;
+		registry.add("spring.data.redis.port", () -> redis.getMappedPort(REDIS_PORT));
+	}
+	
 	@Test
 	void contextLoads() {
 	}
