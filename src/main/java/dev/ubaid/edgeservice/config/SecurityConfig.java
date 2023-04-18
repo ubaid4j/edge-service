800adb5bcf5e22df3c0ea44e6ef482fab0a30bf5
ubaid4j <ubaidshan007@gmail.com>
2023-04-18 15:06:58 +0500

    - add security

--- src/main/java/dev/ubaid/edgeservice/config/SecurityConfig.java
+++ src/main/java/dev/ubaid/edgeservice/config/SecurityConfig.java
@@ -1,17 +1,18 @@
 package dev.ubaid.edgeservice.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
-@Configuration
+@EnableWebFluxSecurity
 public class SecurityConfig {
-
     @Bean
     SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
         return http
             .authorizeExchange(exchange -> exchange.anyExchange().authenticated())
+            .formLogin(Customizer.withDefaults())
             .build();
     }
 }
