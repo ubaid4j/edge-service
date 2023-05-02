bd99e55151f922a01dfc391d9d963bc5eb6ae442
ubaid4j <ubaidshan007@gmail.com>
2023-05-02 10:14:12 +0500

    - code enhancement

--- src/main/java/dev/ubaid/edgeservice/config/SecurityConfig.java
+++ src/main/java/dev/ubaid/edgeservice/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package dev.ubaid.edgeservice.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
@@ -15,6 +16,7 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint;
 import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
 
+@Configuration
 @EnableWebFluxSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
