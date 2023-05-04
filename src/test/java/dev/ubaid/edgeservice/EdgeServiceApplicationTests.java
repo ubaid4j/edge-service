376c2a11ac9ed82e324b3432e0097362584051a6
ubaid4j <ubaidshan007@gmail.com>
2023-05-04 07:04:05 +0500

    - fix test

--- src/test/java/dev/ubaid/edgeservice/EdgeServiceApplicationTests.java
+++ src/test/java/dev/ubaid/edgeservice/EdgeServiceApplicationTests.java
@@ -3,6 +3,8 @@ package dev.ubaid.edgeservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.GenericContainer;
@@ -22,13 +24,16 @@ class EdgeServiceApplicationTests {
 	private static GenericContainer<?> redis =
 		new GenericContainer<>(DockerImageName.parse("redis:latest"))
 			.withExposedPorts(REDIS_PORT);
-	
+
 	@DynamicPropertySource
 	private static void redisProps(DynamicPropertyRegistry registry) {
 		registry.add("spring.data.redis.host", () -> redis.getHost());;
 		registry.add("spring.data.redis.port", () -> redis.getMappedPort(REDIS_PORT));
 	}
-	
+
+	@MockBean
+	ReactiveClientRegistrationRepository reactiveClientRegistrationRepository;
+
 	@Test
 	void contextLoads() {
 	}
