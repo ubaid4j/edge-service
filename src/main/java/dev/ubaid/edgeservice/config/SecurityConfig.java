1ec27f4ec9fe8dd3ee0655c61b839e26edd71947
ubaid4j <ubaidshan007@gmail.com>
2023-07-24 10:43:49 +0500

    - security tests

--- src/main/java/dev/ubaid/edgeservice/config/SecurityConfig.java
+++ src/main/java/dev/ubaid/edgeservice/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity.AuthorizeExchangeSpec;
@@ -59,9 +60,10 @@ public class SecurityConfig {
         return http
             .authorizeExchange(AUTHORIZE_EXCHANGE)
             .oauth2Login(Customizer.withDefaults())
-//            .exceptionHandling(EXCEPTION_HANDLING)
+            .exceptionHandling(EXCEPTION_HANDLING)
             .logout(logout)
             .csrf(CSRF)
+            .oauth2ResourceServer(spec -> spec.jwt(Customizer.withDefaults()))
             .build();
     }
 
